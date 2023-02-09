4c08129a3a18eecee4f3388a1216dfc6223a70e1
xk <1548051137@goocan.net>
2023-02-09 18:20:30 +0800

    swaggwer冲突

--- yasuo-auth/src/main/java/com/yasuo/auth/YasuoAuthSpringApplication.java
+++ yasuo-auth/src/main/java/com/yasuo/auth/YasuoAuthSpringApplication.java
@@ -1,7 +1,9 @@
 package com.yasuo.auth;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Description:
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2023/2/6 14:23
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableKnife4j
 public class YasuoAuthSpringApplication {
 
     public static void main(String[] args) {
